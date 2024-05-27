d5818c394819fe0788fe57192a1872fdda9aecd7
mohit9889 <raj.mohit198@gmail.com>
2024-05-27 20:28:56 +0530

    initial project setup

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+// .eslintrc.js
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  extends: [
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+  ],
+  plugins: ["@typescript-eslint", "prettier"],
+  rules: {
+    "prettier/prettier": "error",
+  },
+};
